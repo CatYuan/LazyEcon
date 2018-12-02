5544f350378f6989336e42699296d120bf576c05
Catherine Yuan <catherine.cj.yuan@gmail.com>
2018-12-02 15:20:29 -0600

    logs added

--- app/src/main/java/cs125/lazyecon/MainActivity.java
+++ app/src/main/java/cs125/lazyecon/MainActivity.java
@@ -3,9 +3,12 @@ package cs125.lazyecon;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +17,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToMicro(View view) {
+        Log.d(LOG_TAG, "micro_button clicked");
         Intent microIntent = new Intent(this, MicroActivity.class);
         startActivity(microIntent);
     }
 
     public void goToMacro(View view) {
+        Log.d(LOG_TAG, "macro_button clicked");
         Intent macroIntent = new Intent(this, MacroActivity.class);
         startActivity(macroIntent);
     }
