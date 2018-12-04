675038b183b405759ccbd8be09f1ad0cdfc5f265
Saum1500 <saumyaa2@illinois.edu>
2018-12-03 20:33:47 -0600

    fragments

--- app/src/main/java/cs125/lazyecon/MicroActivity.java
+++ app/src/main/java/cs125/lazyecon/MicroActivity.java
@@ -79,31 +79,6 @@ public class MicroActivity extends AppCompatActivity {
     }
 
 
-    private LineGraphSeries<DataPoint> series;
-    public void equationtoGraph(String intercept, String slope, boolean isPositive) {
-        int i = Integer.parseInt(intercept);
-        int s = Integer.parseInt(slope);
-        double y;
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        series = new LineGraphSeries<>();
-        int numDataPoints = 500;
-        double x = 0;
-        if (isPositive) {
-            for (int index = 0; index < numDataPoints; index++) {
-                x += 0.1;
-                y = i + (s*x);
-                series.appendData(new DataPoint(x,y), true, 100);
-            }
-            graph.addSeries(series);
-        } else {
-            for (int index = 0; index < numDataPoints; index++) {
-                x += 0.1;
-                y = i - (s*x);
-                series.appendData(new DataPoint(x,y), true, 100);
-            }
-            graph.addSeries(series);
-        }
-    }
 
     public static void main(String[] args) {
         
--- app/src/main/java/cs125/lazyecon/TabFragment4.java
+++ app/src/main/java/cs125/lazyecon/TabFragment4.java
@@ -7,6 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,5 +29,31 @@ public class TabFragment4 extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab_fragment4, container, false);
     }
+    private LineGraphSeries<DataPoint> series;
+    public void equationtoGraph(String intercept, String slope, boolean isPositive) {
+        int i = Integer.parseInt(intercept);
+        int s = Integer.parseInt(slope);
+        double y;
+        series = new LineGraphSeries<>();
+        GraphView graph = (GraphView) getView().findViewById(R.id.graph);
+        int numDataPoints = 500;
+        double x = 0;
+        if (isPositive) {
+            for (int index = 0; index < numDataPoints; index++) {
+                x += 0.1;
+                y = i + (s * x);
+                series.appendData(new DataPoint(x, y), true, 100);
+            }
+            graph.addSeries(series);
+        } else {
+            for (int index = 0; index < numDataPoints; index++) {
+                x += 0.1;
+                y = i - (s * x);
+                series.appendData(new DataPoint(x, y), true, 100);
+            }
+            graph.addSeries(series);
+        }
+    }
+
 
 }
