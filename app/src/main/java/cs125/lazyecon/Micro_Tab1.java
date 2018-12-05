5f8391175b225d222c9db0533d6f265aae508530
Saum1500 <saumyaa2@illinois.edu>
2018-12-05 01:21:19 -0600

    DONE WITH ELASTICITY OF DEMAND

--- app/src/main/java/cs125/lazyecon/Micro_Tab1.java
+++ app/src/main/java/cs125/lazyecon/Micro_Tab1.java
@@ -2,6 +2,7 @@ package cs125.lazyecon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,23 +14,33 @@ import android.widget.TextView;
 public class Micro_Tab1 extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     private Button submit;
     private EditText priceChange;
-    private EditText demandChange;
-    private String priceChange_string;
-    private String demandChange_string;
+    private EditText QChange;
+    private double priceChange_val;
+    private double QChange_val;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_micro__tab1);
 
         //create the submit button
-        submit = (Button)findViewById(R.id.micro_button);
+        submit = (Button)findViewById(R.id.button);
         //create the EditText boxes
-        priceChange = (EditText)findViewById(R.id.micro_tab1_price_change_edit_text);
-        demandChange = (EditText)findViewById(R.id.micro_tab1_demand_change_edit_text);
+        priceChange = (EditText)findViewById(R.id.textView9);
+        QChange = (EditText)findViewById(R.id.textView8);
+        result = (TextView) findViewById(R.id.tvanswer);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                priceChange_val = Double.valueOf(priceChange.getText().toString());
+                QChange_val = Double.valueOf(QChange.getText().toString());
+            }
+        });
 
         // Create the spinner.
-        Spinner spinner = (Spinner) findViewById(R.id.micro_tab1_spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
         if (spinner != null) {
             spinner.setOnItemSelectedListener(this);
         }
@@ -44,7 +55,12 @@ public class Micro_Tab1 extends AppCompatActivity implements AdapterView.OnItemS
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String spinner_item = parent.getItemAtPosition(position).toString();
+        String spinneritem = parent.getItemAtPosition(position).toString();
+        if (spinneritem.equals("Demand")) {
+            double d = elasticityd(QChange_val, priceChange_val);
+            result.setText("The Elasticity of Demand is = " + String.valueOf(d));
+        }
+
 
         //TODO insert code for what happens when a specific spinner_item is selected
         //TODO how do we handle different elasticities within the program
@@ -54,9 +70,8 @@ public class Micro_Tab1 extends AppCompatActivity implements AdapterView.OnItemS
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-
-    public void onSubmit(View view) {
-        priceChange_string = priceChange.getText().toString();
-        demandChange_string = demandChange.getText().toString();
+    public double elasticityd(double Q, double P) {
+        return -Math.abs(Q/P);
     }
+
 }
